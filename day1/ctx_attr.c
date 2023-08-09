9c3c91bc2d3360b8a6c54486a8574851131f9069
Adrian Suciu <adrian.suciu@analog.com>
2023-08-09 15:12:46 +0300

    day1: added initial revision
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

---
+++ day1/ctx_attr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iio.h>
+
+#define URI "ip:192.168.2.1"
+
+int main() {
+
+	unsigned int major;
+	unsigned int minor;
+	char git_tag[8];
+	const char *description;
+	const char *ctx_name;
+	const char *ctx_val;
+	struct iio_context *ctx;
+
+	iio_library_get_version(&major, &minor, git_tag);
+
+	printf("libiio version: %d.%d - %s \n", major,minor,git_tag);
+
+	ctx = iio_create_context_from_uri(URI);
+
+	description = iio_context_get_description(ctx);
+	printf("Description: %s\n" , description);
+	iio_context_get_attr(ctx,0,&ctx_name,&ctx_val); 
+	printf("ctx attr %d: %s - %s\n" ,0, ctx_name, ctx_val);
+	iio_context_destroy(ctx);
+
+	return 0;
+}
---
+++ day1/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello Analog Devices students\n");
+	return 0;
+}
