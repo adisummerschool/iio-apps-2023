05423c8ab715bc346ff473ca7cbf677bd6d42297
Radu Sabau <radu.sabau@analog.com>
2023-08-10 17:27:39 +0300

    day1: Completed all tasks for today
    Cmpleted the tasks, each of them are found in ctx_attr.c, first three
    are commented, las one is builded with cmake in the makefile.

--- day1/ctx_attr.c
+++ day1/ctx_attr.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 #include <iio.h>
 
-#define URI "ip:192.168.2.1"
+#define URI "ip:10.76.84.244"
 
 int main() {
 
 	unsigned int major;
 	unsigned int minor;
+	unsigned int attr_count;
+	unsigned int dev_count;
 	char git_tag[8];
 	const char *description;
 	const char *ctx_name;
 	const char *ctx_val;
+	char *chn_read;
+	const char *chn_attr = "raw";
+	char *cur_attr;
+	char buffer[1];
+	char buffer_read[100] = {};
+
 	struct iio_context *ctx;
+	struct iio_device *dev;
+	struct iio_channel *chn;
 
 	iio_library_get_version(&major, &minor, git_tag);
 
@@ -23,6 +34,106 @@ int main() {
 	printf("Description: %s\n" , description);
 	iio_context_get_attr(ctx,0,&ctx_name,&ctx_val); 
 	printf("ctx attr %d: %s - %s\n" ,0, ctx_name, ctx_val);
+	attr_count = iio_context_get_attrs_count(ctx);
+	//printf("attr_count = %d\n", attr_count);
+	int ret = 0;
+	float temp;
+	int raw, scale, offset;
+	//part 4(last part)
+
+	
+	dev = iio_context_find_device(ctx, "xadc");
+	int n = iio_device_get_channels_count(dev);
+	for(int i = 0; i < n; i++)
+	{
+		chn = iio_device_get_channel(dev, i);
+		if(iio_channel_find_attr(chn, "offset") != NULL)
+		{
+			iio_channel_attr_read(chn, "raw", buffer_read, 100);
+			raw = atoi(buffer_read);
+			iio_channel_attr_read(chn, "scale", buffer_read, 100);
+			scale = atoi(buffer_read);
+			iio_channel_attr_read(chn, "offset", buffer_read, 100);
+			offset = atoi(buffer_read);
+			temp = (float)((raw + offset) * scale);
+			printf("Temp is %.3f\n", temp/1000);
+		}
+		// printf("%d\n", raw);
+		// for(int j = 0; j < iio_channel_get_attrs_count(chn); j++)
+		// {
+		// 	printf("%s\n",iio_channel_get_attr(chn, j));
+		// }
+	}
+
+	//part 3
+
+	/*
+	dev = iio_context_get_device(ctx, 0);
+	int chn_cnt = iio_device_get_channels_count(dev);
+	chn = iio_device_get_channel(dev, 0);
+	iio_channel_attr_write(chn, chn_attr,"0");
+	chn = iio_device_get_channel(dev, 1);
+	iio_channel_attr_write(chn, chn_attr,"1");
+	chn = iio_device_get_channel(dev, 2);
+	iio_channel_attr_write(chn, chn_attr,"2");
+	chn = iio_device_get_channel(dev, 3);
+	iio_channel_attr_write(chn, chn_attr,"3");
+	chn = iio_device_get_channel(dev, 4);
+	iio_channel_attr_write(chn, chn_attr,"4");
+	chn = iio_device_get_channel(dev, 5);
+	iio_channel_attr_write(chn, chn_attr,"5");
+	for(int i = 0; i < chn_cnt; i++)
+	{
+		chn = iio_device_get_channel(dev, i);
+		iio_channel_attr_read(chn, chn_attr, buffer_read, 100);
+		printf("Wrote value : %s\n", buffer_read);
+	}
+	*/
+
+	//part 1-2
+
+	/*
+	for(int i = 0; i < attr_count; i++)
+	{
+		ret = iio_context_get_attr(ctx, i, &ctx_name, &ctx_val);
+		if(ret)
+		{
+			printf("Attribute doesn't exist.");
+			return 0;
+		}
+		printf("ctx attr no.%d %d: %s - %s\n", i ,i, ctx_name, ctx_val);
+	}
+	dev_count = iio_context_get_devices_count(ctx);
+	for(int i = 0; i < dev_count; i++)
+	{
+		printf("Device %d\n", i);
+		dev = iio_context_get_device(ctx, i);
+		if(dev)
+		{
+			for(int j = 0; j < iio_device_get_attrs_count(dev); j++)
+			{
+				printf("%s\n", iio_device_get_attr(dev, j));
+			}
+			if(iio_device_get_channels_count(dev) > 0)
+			{
+				printf("Channels :\n");
+				for(int k = 0; k < iio_device_get_channels_count(dev); k++)
+				{
+					chn = iio_device_get_channel(dev, k);
+					chn_attr = iio_channel_get_attr(chn, 0);
+					//iio_channel_attr_read(chn, chn_attr, chn_read, strlen(chn_attr));
+					//printf("raw = %s\n", chn_attr);
+				}
+			}
+		}
+		else
+		{
+			printf("The device doesn't exist.");
+			return 0;
+		}
+	}
+	*/
+
 	iio_context_destroy(ctx);
 
 	return 0;
--- day1/hello.c
+++ day1/hello.c
@@ -2,6 +2,14 @@
 
 int main() {
 
-	printf("Hello Analog Devices students\n");
+	int n = 10;
+
+	for(int i = 0; i < n; i++)
+	{
+		printf("%d\n", i);
+		sleep(2);
+	}
+
+	//printf("Hello Analog Devices students\n");
 	return 0;
 }
