6bc3d04fddb73f35b05b365524f55f9402d82cae
Voic Andrei <voicandrei22@gmail.com>
2023-08-10 17:00:40 +0300

    day1:Implement Day 1 XADC and AD5592R_S Tasks
    - Added functionality to read XADC temperature.
    - Added code to read all attributes across devices and channels.
    - Implemented periodic write to each channel of AD5592R_S, cycling values from 1 to 5 every second.

---
+++ day1/build/CMakeFiles/read_all_channels.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_all_channels.dir/read_all_channels.c.o"
+  "read_all_channels"
+  "read_all_channels.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/read_all_channels.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- day1/hello.c
+++ day1/hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-
-	printf("Hello Analog Devices students\n");
+	int n;
+	printf("Enter the value for n: "); // Prompt the user
+	scanf("%d", &n);                   // Read the input into n
+	for (int i=0;i<=n;i++)
+	{printf("%d\n",i);
+	sleep(1);	}
+	printf("\n");
 	return 0;
 }
