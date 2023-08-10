eaaffc469bd1741e9bf9c2c8fb5402eed083e0ee
ValentinDolcescu7 <valentindolcescu.vd@gmail.com>
2023-08-10 17:55:52 +0300

    ctx_attr&r/w attr  Challenge:  -printed message when context is not available  -readed all context attributes  -readed device/channel attributes

--- day1/ctx_attr.c
+++ day1/ctx_attr.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <iio.h>
+#include <unistd.h>
 
-#define URI "ip:192.168.2.1"
+#define URI "ip:10.76.84.158"
 
 int main() {
-
+        
+	unsigned int ctx_count;
 	unsigned int major;
 	unsigned int minor;
 	char git_tag[8];
@@ -12,17 +14,48 @@ int main() {
 	const char *ctx_name;
 	const char *ctx_val;
 	struct iio_context *ctx;
+	const struct iio_device *dev;
+	const char *name;
+	const struct iio_channel *chan;
 
 	iio_library_get_version(&major, &minor, git_tag);
 
 	printf("libiio version: %d.%d - %s \n", major,minor,git_tag);
 
 	ctx = iio_create_context_from_uri(URI);
-
+        if(ctx == NULL)
+	{
+		     printf("Check IP address!");
+		     return 0;
+	}
 	description = iio_context_get_description(ctx);
 	printf("Description: %s\n" , description);
-	iio_context_get_attr(ctx,0,&ctx_name,&ctx_val); 
-	printf("ctx attr %d: %s - %s\n" ,0, ctx_name, ctx_val);
+  
+        ctx_count = iio_context_get_attrs_count(ctx);
+	printf("ctx numbers: %d", ctx_count); 
+
+	for (int i=0;i<ctx_count;i++)
+	{    
+	     	
+                iio_context_get_attr(ctx,i,&ctx_name,&ctx_val); 
+	        printf("\nctx attr %d: %s - %s\n" ,i, ctx_name, ctx_val);
+	}
+        
+        dev = iio_context_get_device(ctx, 0); 
+	name = iio_device_get_name(dev);
+	unsigned numbers_of_channels = iio_device_get_channels_count(dev);
+	printf("name: %s\n", name);
+	printf("numbers of channels: %d\n", numbers_of_channels);
+	chan = iio_device_find_channel(dev, "voltage0", false);
+	char tmp[10];
+
+	for(int j=0;j<4;j++)
+	    {       
+		    iio_channel_attr_read(chan, "raw", tmp, 10);
+                    printf("%s\n", tmp);
+	    }
+
+         
 	iio_context_destroy(ctx);
 
 	return 0;
--- day1/hello.c
+++ day1/hello.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
 
 	printf("Hello Analog Devices students\n");
+	int n = 10;
+	for (int i=0;i<=n;i++)
+	{
+           printf("%d", i);
+	   printf("\n");
+	   sleep(1);
+	}
 	return 0;
 }
