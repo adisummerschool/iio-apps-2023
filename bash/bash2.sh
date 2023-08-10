e641f179de8dff1e3231a7cca152119bac3d0532
Radu Sabau <radu.sabau@analog.com>
2023-08-10 14:09:59 +0300

    bash : Added bash file with 2 scripts
    Added two bash scripts, each of them correspnds to a certain task
    that was assigned.

---
+++ bash/bash2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+iio_attr -u ip:10.76.84.244 -c ad5592r_s voltage0 raw 0
+iio_attr -u ip:10.76.84.244 -c ad5592r_s voltage1 raw 1
+iio_attr -u ip:10.76.84.244 -c ad5592r_s voltage2 raw 2
+iio_attr -u ip:10.76.84.244 -c ad5592r_s voltage3 raw 3
+iio_attr -u ip:10.76.84.244 -c ad5592r_s voltage4 raw 4
+iio_attr -u ip:10.76.84.244 -c ad5592r_s voltage5 raw 5
