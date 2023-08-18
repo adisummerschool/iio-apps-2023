ad8192a263cf824adf6eed967a519a826e7d39e7
Analog <adinailisei6>
2023-08-18 10:51:20 +0300

    iio: Add message when context is not available and quit.

--- day1/ctx_attr.c
+++ day1/ctx_attr.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <iio.h>
 
-#define URI "ip:192.168.2.1"
+#define URI "ip:10.76.84.156"
 
 int main() {
 
@@ -19,11 +19,20 @@ int main() {
 
 	ctx = iio_create_context_from_uri(URI);
 
+	//Message when context is not available and quit
+	if(ctx == NULL)
+	{
+		printf("ip gresit!\n");
+		return 0;
+	}
+
 	description = iio_context_get_description(ctx);
 	printf("Description: %s\n" , description);
 	iio_context_get_attr(ctx,0,&ctx_name,&ctx_val); 
-	printf("ctx attr %d: %s - %s\n" ,0, ctx_name, ctx_val);
+	printf("ctx attr %d: %s - %s\n" ,0, ctx_name, ctx_val);	
+
 	iio_context_destroy(ctx);
 
 	return 0;
+
 }
--- day1/hello.c
+++ day1/hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
 
-	printf("Hello Analog Devices students\n");
+	int n = 10;
+	for(int i=0;i<n;i++)
+	{	printf("%d\n",i);
+		sleep(1);
+	}
+	//printf("Hello Analog Devices students\n");
 	return 0;
 }
