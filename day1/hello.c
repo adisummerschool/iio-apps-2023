14913858924ca3b6939ff6d45de15dbc4da20bf7
Bontya Thomas <thomasbontya@gmail.com>
2023-08-18 10:47:34 +0300

    day1/build Build File
    Am creat fisierul build impreuna cu make-ul si cmake-ul.
    Am creat un program care sa ne afiseze canalele cu valorile lui.

--- day1/hello.c
+++ day1/hello.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
+	char c = *argv[1];
+	int n = c - '0' ;
+
+	for(int i=0; i<=n; i++)
+	{
+		
 	printf("Hello Analog Devices students\n");
+	
+	}
+
 	return 0;
 }
