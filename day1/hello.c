adceab9e4fda1d7d2b43897dd5287b7c98f7adf2
EmmaSiminaMuresan <simina.muresan@outlook.com>
2023-08-18 11:45:27 +0300

    iio-apps-2023/day1 : Add a new C program that implements a countdown loop with delays and a friendly greeting.
    The newly added C program consists of a loop that takes an integer input 'n' and counts from 0 to 'n-1'. Each count is printed on a new line, and there is a 1-second delay between each count using the 'sleep' function from the 'unistd' library. After the countdown, the program concludes by displaying a greeting message to the Analog Devices students.

--- day1/hello.c
+++ day1/hello.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
+	int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", i);
+		sleep(1);
+    }
+
 
 	printf("Hello Analog Devices students\n");
 	return 0;
