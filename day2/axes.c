b2e3fb5f325ea886333687535c08bff8e81e005a
EmmaSiminaMuresan <simina.muresan@outlook.com>
2023-08-18 14:11:19 +0300

    day2 : Read raw, voltage and acceleration of each axis
    The program creates a context from the specified URI and checks if the creation was successful. If a valid context is created, the program proceeds to retrieve the count of devices available.
    
    For each device, the program retrieves the channels associated with it and displays information about these channels. The channel names, such as "xpoz", "xneg", "ypoz", etc., are pre-defined and used to identify specific channels. The program reads the "raw" attribute from each channel, converts it to integer values, and calculates corresponding voltage and acceleration values. These values are then displayed on the console.
    
    In cases where a channel is not available, the program prints an appropriate message.
    
    After processing all available devices and channels, the program destroys the context and returns 0 to indicate successful execution.

---
+++ day2/axes.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <iio.h>
+#include <unistd.h>
+
+#define URI "ip:10.76.84.239"
+
+int main() {
+    unsigned int major;
+    unsigned int minor;
+    char git_tag[8];
+    struct iio_context *ctx;
+
+    iio_library_get_version(&major, &minor, git_tag);
+    printf("libiio version: %d.%d - %s\n", major, minor, git_tag);
+
+    ctx = iio_create_context_from_uri(URI);
+    if (ctx == NULL) {
+        fprintf(stderr, "Failed to create context from URI: %s\n", URI);
+        return -1;
+    }
+
+    int dev_count = iio_context_get_devices_count(ctx);
+
+    if (dev_count > 0) {
+        struct iio_device *dev = iio_context_get_device(ctx, 0);
+
+        const char* axis_names[] = {
+            "xpoz",
+            "xneg",
+            "ypoz",
+            "yneg",
+            "zpoz",
+            "zneg"
+        };
+
+        int ch_count = iio_device_get_channels_count(dev);
+
+        // Display channels in the specified order
+        for (int j = 0; j < 6; j++) {
+            if (j < ch_count) {
+                struct iio_channel *ch = iio_device_get_channel(dev, j);
+            
+                char attr_val[1024];
+                iio_channel_attr_read(ch, "raw", attr_val, sizeof(attr_val));
+                int raw = atoi(attr_val);
+
+                double volts = (double)raw / 1638.0;
+                double acceleration = (double)raw / 4095.0;
+
+                printf("%s %d %.4lf %.4lf\n", axis_names[j], raw, volts, acceleration);
+            } else {
+                printf("%s Not Available\n", axis_names[j]);
+            }
+        }
+    } else {
+        fprintf(stderr, "No devices found.\n");
+    }
+
+    iio_context_destroy(ctx);
+    return 0;
+}
