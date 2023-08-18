f94ca06ebc81470a04d1d9a989b01d57c3bcfc64
Vlad <vladcristescu1@gmail.com>
2023-08-18 11:29:21 +0300

    iio-apps-2023: day1: Manipulate attributes
    Printed err msg when context is not available, read all context
    attr, read all device/channel attr, read xadc temp

--- day1/ctx_attr.c
+++ day1/ctx_attr.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <iio.h>
+#include <unistd.h>
 
-#define URI "ip:192.168.2.1"
+#define URI "ip:10.76.84.201"
 
 int main() {
 
@@ -12,17 +13,60 @@ int main() {
 	const char *ctx_name;
 	const char *ctx_val;
 	struct iio_context *ctx;
+	const struct iio_device *dev;
+	const char *attr = "en";
+	char dst[8];
+	size_t len = 8;
 
 	iio_library_get_version(&major, &minor, git_tag);
 
-	printf("libiio version: %d.%d - %s \n", major,minor,git_tag);
-
 	ctx = iio_create_context_from_uri(URI);
+	
+	dev = iio_context_find_device(ctx, "ad5592r_s");
+
+	int dev_count = iio_context_get_devices_count(ctx);
+
+	if(!ctx){
+		printf("ERROR: Context not available");
+		return 0;
+	}
+
+	for (int i = 0; i < iio_context_get_attrs_count(ctx); i++){ 
+		iio_context_get_attr(ctx, i, &ctx_name, &ctx_val);
+		printf("Context attributes: %d - %s -> %s \n", i, ctx_name, ctx_val);
+	}
+
+	for (int i = 0; i < iio_device_get_attrs_count(dev); i++){ 
+		iio_device_attr_read(dev, "en", dst, len);
+		printf("Device attributes: %d - %s \n", i, attr);
+	}
+
+	for (int i = 0; i < dev_count; i++) {
+        	struct iio_device *dev = iio_context_get_device(ctx, i);
+        	const char *dev_name = iio_device_get_name(dev);
+
+        	if (dev_name && strcmp(dev_name, "xadc") == 0) {
+            		int ch_count = iio_device_get_channels_count(dev);
+            		for (int j = 0; j < ch_count; j++) {
+               			struct iio_channel *ch = iio_device_get_channel(dev, j);
+
+                		if (strcmp(iio_channel_get_id(ch), "temp0") == 0) {
+                    			long long raw, offset;
+                    			double scale;
+
+                    			iio_channel_attr_read_longlong(ch, "raw", &raw);
+                    			iio_channel_attr_read_longlong(ch, "offset", &offset);
+                    			iio_channel_attr_read_double(ch, "scale", &scale);
+
+                   			 double temp = (raw + offset) * scale;
+                   			 printf("Temperature: %.2f\n", temp);
+                		}
+            		}
+       		}		
+    	}
 
 	description = iio_context_get_description(ctx);
 	printf("Description: %s\n" , description);
-	iio_context_get_attr(ctx,0,&ctx_name,&ctx_val); 
-	printf("ctx attr %d: %s - %s\n" ,0, ctx_name, ctx_val);
 	iio_context_destroy(ctx);
 
 	return 0;
--- day1/hello.c
+++ day1/hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <time.h>
+#include <unistd.h>
 
 int main() {
 
-	printf("Hello Analog Devices students\n");
+	int n = 10;
+
+	for(int i = 0; i < n; i++){
+		printf("Hello Analog Devices student %d \n", i);
+		sleep(1);
+	}
 	return 0;
 }
