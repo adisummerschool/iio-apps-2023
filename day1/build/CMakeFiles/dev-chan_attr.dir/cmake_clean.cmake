f90815020a3dc01216e14d3aaba2125b0f836fe6
Vlad <vladcristescu1@gmail.com>
2023-08-21 15:00:54 +0300

    libiio: iio-apps-2023: Complete daily challenges
    read_val: display values in bits, volts, g's and m/s^2
    read_accel: display total acc in a loop
    calib_accel: fine-tune the potentiometers
    shock_detect: track the timestamps of shocks

---
+++ day1/build/CMakeFiles/dev-chan_attr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dev-chan_attr.dir/dev-chan_attr.c.o"
+  "dev-chan_attr"
+  "dev-chan_attr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dev-chan_attr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ day1/build/CMakeFiles/read_val.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/read_val.dir/read_val.c.o"
+  "read_val"
+  "read_val.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/read_val.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
