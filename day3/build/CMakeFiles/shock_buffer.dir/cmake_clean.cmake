3dba409a9e91d666925998c21cf5122e690bbaf6
Radu Sabau <radu.sabau@analog.com>
2023-08-23 18:00:57 +0300

    day 3: Did a shock_buffer program in c and a waveform in python

---
+++ day3/build/CMakeFiles/shock_buffer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shock_buffer.dir/shock_buffer.c.o"
+  "shock_buffer"
+  "shock_buffer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shock_buffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
